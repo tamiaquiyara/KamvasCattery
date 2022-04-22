ae35f9b099f23d585cdcd43de99f615ca28716ce
Tamia Quiyara <mia.mbuckingworth@gmail.com>
2022-04-22 15:43:33 +0200

    Added 3 tests

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,15 @@
+describe('This bookingCost function' , function(){
+    it('should calculate the booking request cost for Monday to Wednesday' , function(){
+        assert.equal(135,bookingCost("Monday","3"));
+    });
 
+    it('should calculate the booking request cost for Wednesday to Friday'  , function(){
+        assert.equal(101.25,bookingCost("Wednesday","3"*"0.75"));
+    });
+
+    it('should calculate the booking request cost for Friday to Sunday' , function(){
+        assert.equal(114.75,bookingCost("45*3"));
+    });
+
+
+});
